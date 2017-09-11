4ab2c136d91a2b23c945113bc73c56245e6f67d9
Ben Rotter <BenJRotter@gmail.com>
2017-09-11 10:47:10 -0500

    added useful echos and also suppressed all the garbage from the second compile so I can actually see issues

--- makeThesis.sh
+++ makeThesis.sh
@@ -6,6 +6,9 @@
 #
 #########
 
+echo "makeThesis():  Making aux file."
 pdflatex --draft thesis #draft will only produce the aux file
+echo "makeThesis(): Creating Bibliography."
 bibtex thesis
-pdflatex thesis
+echo "makeThesis(): Remaking thesis, with only errors and warnings shown"
+pdflatex thesis | grep -i 'warning\|error'
